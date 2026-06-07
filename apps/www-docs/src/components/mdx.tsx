6131dac0110db069ddec1ea33b8ede265d33d4d5
Marcello Novelli <62885851+mnove@users.noreply.github.com>
2026-06-07 16:45:25 +0200

    Add docs (#1)
    * Update docs
    
    * edit
    
    * Fix config loader

--- apps/www-docs/src/components/mdx.tsx
+++ apps/www-docs/src/components/mdx.tsx
@@ -1,9 +1,17 @@
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { TypeTable } from "fumadocs-ui/components/type-table";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
+    Tab,
+    Tabs,
+    Step,
+    Steps,
+    TypeTable,
     ...components,
   } satisfies MDXComponents;
 }
