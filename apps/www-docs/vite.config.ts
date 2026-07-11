bc400cf106e9dbc979192f4f0592472349b03b69
mnove <novelli3000@gmail.com>
2026-07-11 18:13:58 +0200

    Fix prerendering issue

--- apps/www-docs/vite.config.ts
+++ apps/www-docs/vite.config.ts
@@ -6,6 +6,10 @@ import react from "@vitejs/plugin-react";
 import mdx from "fumadocs-mdx/vite";
 import { defineConfig } from "vite";
 
+if (process.env.TURBO_HASH) {
+  process.env.CI = "true";
+}
+
 export default defineConfig({
   server: {
     port: 3000,
